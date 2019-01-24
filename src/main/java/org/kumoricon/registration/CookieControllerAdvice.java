e5516482e5fb88dfbf849956942a12197ebfb697
Jason Short <jason@sheersky.com>
2019-01-23 21:54:42 -0800

    Added prototype printer selection page w/ cookie support

---
+++ src/main/java/org/kumoricon/registration/CookieControllerAdvice.java
@@ -0,0 +1,43 @@
+package org.kumoricon.registration;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * This class gets information from various cookies and injects it in to the Thymeleaf model
+ * class, so they can be used directly in all templates.
+ */
+@ControllerAdvice
+public class CookieControllerAdvice {
+    public static final String PRINTER_COOKIE_NAME = "PRINTERNAME";
+    public static final String TILL_COOKIE_NAME = "TILLNAME";
+
+    @ModelAttribute("selectedPrinter")
+    public String selectedPrinter(final HttpServletRequest request) {
+        for (Cookie c : request.getCookies()) {
+            if (c.getName().equals(PRINTER_COOKIE_NAME)) {
+                return c.getValue();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Provides the till number, or null if empty. Till is typically just a number
+     * @param request
+     * @return
+     */
+    @ModelAttribute("selectedTill")
+    public String selectedTill(final HttpServletRequest request) {
+        for (Cookie c : request.getCookies()) {
+            if (c.getName().equals(TILL_COOKIE_NAME)) {
+                return c.getValue();
+            }
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/org/kumoricon/registration/utility/PrinterController.java
@@ -0,0 +1,51 @@
+package org.kumoricon.registration.utility;
+
+
+import org.kumoricon.registration.CookieControllerAdvice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class PrinterController {
+
+
+    @Autowired
+    public PrinterController() {
+
+    }
+
+    @RequestMapping(value = "/utility/printer")
+    public String printer(Model model,
+                          @CookieValue(value = CookieControllerAdvice.PRINTER_COOKIE_NAME, required = false) String printerName,
+                          @RequestParam(required = false) String msg) {
+        model.addAttribute("printer", printerName);
+        model.addAttribute("msg", msg);
+        return "utility/printer";
+    }
+
+    @RequestMapping(value = "/utility/printer", method = RequestMethod.POST)
+    public String setPrinter(Model model, @RequestParam String printer, HttpServletResponse response) {
+        String newPrinter = printer.trim();
+        try {
+            model.addAttribute("printer", newPrinter);
+            model.addAttribute("msg", "Selected printer \"" + newPrinter + "\"");
+            Cookie cookie = new Cookie(CookieControllerAdvice.PRINTER_COOKIE_NAME, newPrinter);
+            cookie.setPath("/");
+            cookie.setHttpOnly(true);
+            response.addCookie(cookie);
+        } catch (Exception e) {
+            model.addAttribute("err", e.getMessage());
+
+        }
+
+        return "redirect:/utility/printer?msg=Printer+Saved";
+    }
+}
