e512284f22d785b36972e271a4b39d20b8c741ca
Kent McKinney <kent.p.mckinney@gmail.com>
2019-02-17 10:43:13 -0800

    Created a DRAFT of the Staff Report

---
+++ src/main/java/org/kumoricon/registration/model/user/StaffRepository.java
@@ -0,0 +1,15 @@
+package org.kumoricon.registration.model.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface StaffRepository extends JpaRepository<User, Integer> {
+    List<User> findByLastNameStartsWithIgnoreCase(String lastName);
+    User findOneByUsernameIgnoreCase(String username);
+    User findOneById(Integer id);
+
+    User findOneByUuid(String uuid);
+}
