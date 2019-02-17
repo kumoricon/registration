c771482d0d90bb6d9b9d28f96bacbefa30adc56d
Kent McKinney <kent.p.mckinney@gmail.com>
2019-02-17 13:11:50 -0800

    Created a DRAFT of the Roles Report

---
+++ src/main/java/org/kumoricon/registration/reports/RolesReportController.java
@@ -0,0 +1,27 @@
+package org.kumoricon.registration.reports;
+
+import org.kumoricon.registration.model.role.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class RolesReportController {
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public RolesReportController(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @RequestMapping(value = "/reports/roles")
+    @PreAuthorize("hasAuthority('view_check_in_by_hour_report')")
+    public String listRoles(Model model) {
+        model.addAttribute("data", roleRepository.listRoles());
+        return "reports/roles";
+    }
+
+}
