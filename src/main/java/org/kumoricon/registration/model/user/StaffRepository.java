64daa768678dd8bc12e414253465fc0c80c21002
Kent McKinney <kent.p.mckinney@gmail.com>
2019-02-17 20:02:29 -0800

    Created a DRAFT of the Till Sessions Report

--- src/main/java/org/kumoricon/registration/model/user/StaffRepository.java
+++ src/main/java/org/kumoricon/registration/model/user/StaffRepository.java
@@ -1,6 +1,7 @@
 package org.kumoricon.registration.model.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,6 +11,8 @@ public interface StaffRepository extends JpaRepository<User, Integer> {
     List<User> findByLastNameStartsWithIgnoreCase(String lastName);
     User findOneByUsernameIgnoreCase(String username);
     User findOneById(Integer id);
-
     User findOneByUuid(String uuid);
+
+    @Query(value = "SELECT first_name, last_name, username, id, (SELECT roles.name FROM roles WHERE roles.id = role_id) AS Role FROM users", nativeQuery = true)
+    List<User> findAll();
 }
