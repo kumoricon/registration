d3cbe3917003d7df81c5f70d6c258fa6d242cd11
Jason <jason@sheersky.com>
2019-03-09 11:23:47 -0800

    Changed Staff Report to use JdbcTemplate calls

--- src/main/java/org/kumoricon/registration/reports/StaffReportController.java
+++ src/main/java/org/kumoricon/registration/reports/StaffReportController.java
@@ -1,44 +1,35 @@
 package org.kumoricon.registration.reports;
 
-//import org.kumoricon.registration.model.user.StaffRepository;
+import org.kumoricon.registration.model.user.User;
+import org.kumoricon.registration.model.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.ArrayList;
 import java.util.List;
 
 
 @Controller
 public class StaffReportController {
-//    private final StaffRepository staffRepository;
+    private final UserRepository userRepository;
 
-//    @Autowired
-//    public StaffReportController(StaffRepository staffRepository) {
-//        this.staffRepository = staffRepository;
-//    }
-    public StaffReportController() {
-//        this.staffRepository = null;
+    @Autowired
+    public StaffReportController(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
+
 
     @RequestMapping(value = "/reports/staff")
     @PreAuthorize("hasAuthority('view_staff_report')")
     public String staff(Model model,
                         @RequestParam(required=false) String err,
                         @RequestParam(required=false) String msg) {
-        try {
-//            List<Object> users = staffRepository.findAllStaff();
-            model.addAttribute("staff", new ArrayList<>());
-            model.addAttribute("err", err);
-        } catch (NumberFormatException ex) {
-            model.addAttribute("err", ex.getMessage());
-        }
-
+        List<User> users = userRepository.findAll();
+        model.addAttribute("staff", users);
+        model.addAttribute("err", err);
         model.addAttribute("msg", msg);
-
         return "reports/staff";
     }
 }
