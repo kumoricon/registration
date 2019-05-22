a5aa8fe1fbf1aecfd8e8d66662c33d45fda692f3
Jason Short <jason@sheersky.com>
2019-05-21 21:05:05 -0700

    Added till session cookie

--- src/main/java/org/kumoricon/registration/utility/TillSessionController.java
+++ src/main/java/org/kumoricon/registration/utility/TillSessionController.java
@@ -1,21 +1,29 @@
 package org.kumoricon.registration.utility;
 
 
+import org.kumoricon.registration.controlleradvice.CookieControllerAdvice;
 import org.kumoricon.registration.model.tillsession.TillSessionDTO;
 import org.kumoricon.registration.model.tillsession.TillSessionService;
 import org.kumoricon.registration.model.user.User;
 import org.kumoricon.registration.model.user.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.security.Principal;
 
 @Controller
 public class TillSessionController {
     private final TillSessionService tillSessionService;
     private final UserService userService;
+    private static final Logger log = LoggerFactory.getLogger(TillSessionController.class);
 
     public TillSessionController(TillSessionService tillSessionService, UserService userService) {
         this.tillSessionService = tillSessionService;
@@ -23,23 +31,56 @@ public class TillSessionController {
     }
 
     @RequestMapping(value = "/utility/till")
-    public String getTillSession(Model model, Principal user) {
+    public String getTillSession(Model model,
+                                 @CookieValue(value = CookieControllerAdvice.TILL_COOKIE_NAME, required = false) String tillName,
+                                 Principal user) {
         User currentUser = userService.findByUsername(user.getName());
         if (currentUser == null) throw new RuntimeException("User not found");
 
+
+
         TillSessionDTO s = tillSessionService.getOpenSessionForUser(currentUser);
 
+        model.addAttribute("tillName", tillName);
         model.addAttribute("tillSession", s);
         return "utility/tillsession";
     }
 
     @RequestMapping(value = "/utility/till", method = RequestMethod.POST)
-    public String closeTill(Model model, Principal user) {
+    public String closeTill(Model model,
+                            Principal user,
+                            @RequestParam String action,
+                            @RequestParam String tillName,
+                            HttpServletResponse response) {
+
         User currentUser = userService.findByUsername(user.getName());
         if (currentUser == null) throw new RuntimeException("User not found");
 
-        tillSessionService.closeSessionForUser(currentUser);
+        String sanitizedTillName = tillName.trim();
 
-        return "redirect:/utility/till?msg=Till%20Session%20Closed";
+        if (action.equals("Save")) {
+            log.info("Setting till to {}", tillName);
+            try {
+                Cookie cookie = new Cookie(CookieControllerAdvice.TILL_COOKIE_NAME, sanitizedTillName);
+                cookie.setPath("/");
+                cookie.setHttpOnly(true);
+                response.addCookie(cookie);
+                return "redirect:/utility/till?msg=Set+till+" + sanitizedTillName;
+
+            } catch (Exception e) {
+                model.addAttribute("tillName", tillName);
+                model.addAttribute("tillSession", tillSessionService.getOpenSessionForUser(currentUser));
+
+                log.error("Error setting printer to {}", sanitizedTillName, e);
+                model.addAttribute("err", e.getMessage());
+                return "utility/tillsession";
+            }
+        } else if (action.equals("Close")) {
+            tillSessionService.closeSessionForUser(currentUser);
+
+            return "redirect:/utility/till?msg=Till%20Session%20Closed";
+        }
+
+        throw new RuntimeException("Action not specified");
     }
 }
