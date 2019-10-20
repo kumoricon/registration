092ba6eecff3f67f8f2e8023598caa12e5aa1080
Jason Short <jason@sheersky.com>
2019-10-19 18:28:35 -0700

    Hide access denied stacktrace in logs

--- src/main/java/org/kumoricon/registration/ErrorController.java
+++ src/main/java/org/kumoricon/registration/ErrorController.java
@@ -3,6 +3,7 @@ package org.kumoricon.registration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,12 +18,19 @@ public class ErrorController {
     @ExceptionHandler(Throwable.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String exception(final Throwable throwable, final Model model, final HttpServletRequest request) {
-        logger.error("[{}] on {} calling [{}] got exception: {}",
-                request.getUserPrincipal().getName(),
-                request.getRemoteAddr(),
-                request.getRequestURI(),
-                throwable,
-                throwable);
+        if (AccessDeniedException.class == throwable.getClass()) {
+            logger.error("on {} calling [{}] got exception: {}",
+                    request.getRemoteAddr(),
+                    request.getRequestURI(),
+                    throwable.getMessage());
+        } else {
+            logger.error("{} calling [{}] got exception: {}",
+                    request.getRemoteAddr(),
+                    request.getRequestURI(),
+                    throwable.getMessage(),
+                    throwable);
+
+        }
 
         String errorMessage = (throwable != null ? throwable.getMessage() : "Unknown error");
         model.addAttribute("err", errorMessage);
