a03f24f14a29d5ddaaa782cd0b95f75c661f0acf
calebdederick <caleb.dederick@hotmail.com>
2020-02-26 21:52:03 -0800

    Refactor payment templates and allow check number to have minimum 1 digit entered.

---
+++ src/main/resources/static/js/payment/cardPayment.js
@@ -0,0 +1,58 @@
+$(document).ready(
+    function(){
+        setState();
+        addListeners();
+    }
+);
+
+function setState() {
+    var btnSave = $('#btnSave');
+    var inputAmount = $('#inputAmount');
+
+    var amount = parseFloat(inputAmount.val());
+    var due = parseFloat($('#amountDue').val());
+
+    if (isNaN(amount) || isNaN(due)) {
+        btnSave.attr("disabled", true);
+        return;
+    }
+
+    if (amount <= 0) {
+        inputAmount.val("").select();
+        btnSave.attr("disabled", true);
+        alert("Payments must be a positive number");
+        return;
+    }
+
+    // Make sure auth number is filled in if the field exists (cash and check payment types)
+    var inputAuth = document.getElementById('inputCheckAuth');
+
+    if (inputAuth == null) {
+        inputAuth =  document.getElementById('inputAuthNumber');
+    }
+    if (inputAuth != null) {
+        if (!inputAuth.value || inputAuth.value.length < 5) {
+            btnSave.attr("disabled", true);
+            return;
+        } else {
+            btnSave.attr("disabled", false);
+        }
+    }
+
+    if (inputAuth != null) {
+        if (amount > due) {
+            alert("Do not give change for credit cards or checks\nAmount taken must be less than or equal to amount due");
+            btnSave.attr("disabled", true);
+            return;
+        }
+    }
+
+    btnSave.attr("disabled", false);
+}
+
+function addListeners() {
+    $('#inputAmount').keyup(setState);
+
+    $('#inputCheckAuth').keyup(setState);
+    $('#inputAuthNumber').keyup(setState);
+}
