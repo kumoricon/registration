50a2241c752bee13a6096dd981fd8db5eabdd933
Jason Short <jason@sheersky.com>
2020-02-29 16:05:25 -0800

    Display Rights in Columns on user info screen

--- src/main/java/org/kumoricon/registration/utility/UserInfoController.java
+++ src/main/java/org/kumoricon/registration/utility/UserInfoController.java
@@ -1,11 +1,16 @@
 package org.kumoricon.registration.utility;
 
+import org.kumoricon.registration.model.role.Right;
+import org.kumoricon.registration.model.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Set;
+import java.util.TreeSet;
 
 
 @Controller
@@ -16,8 +21,10 @@ public class UserInfoController {
     }
 
     @RequestMapping(value = "/utility/userinfo")
-    public String admin(Model model, HttpServletRequest request) {
+    public String admin(Model model, HttpServletRequest request, @AuthenticationPrincipal User principal) {
+        Set<Right> rights = new TreeSet<>(principal.getRights());
         model.addAttribute("clientIp", getClientIp(request));
+        model.addAttribute("rights", rights);
         return "utility/userinfo";
     }
 
