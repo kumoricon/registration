6233fff4b0dc438b6ff98caa5aebac2ae2fe3a9b
Caleb <53846457+belac9001@users.noreply.github.com>
2021-08-01 18:51:48 -0700

    Create users for multiple role titles (#103)
    * Create users for multiple role titles
    
    * Updated test_staff with a pathological case
    
    Three different registration staff with similar initials:
    
    Jennifer Armstrong
    Jen Armstrong
    Jan Armstrong
    
    (But yes, this has actually happened)
    
    
    Co-authored-by: Jason Short <jason@sheersky.com>

--- src/main/java/org/kumoricon/registration/staff/staffimport/StaffImportUserCreateService.java
+++ src/main/java/org/kumoricon/registration/staff/staffimport/StaffImportUserCreateService.java
@@ -3,12 +3,16 @@ package org.kumoricon.registration.staff.staffimport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.kumoricon.registration.model.role.Role;
 import org.kumoricon.registration.model.role.RoleRepository;
 import org.kumoricon.registration.model.user.User;
 import org.kumoricon.registration.model.user.UserRepository;
 import org.kumoricon.registration.model.user.UserService;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Map.entry;
 
 
 @Component
@@ -17,6 +21,7 @@ public class StaffImportUserCreateService {
     private final RoleRepository roleRepository;
     private final UserRepository userRepository;
     private final UserService userService;
+    private final Map<String, String> regRoleMapping;
 
     public StaffImportUserCreateService(RoleRepository roleRepository,
                                         UserRepository userRepository,
@@ -24,11 +29,43 @@ public class StaffImportUserCreateService {
         this.roleRepository = roleRepository;
         this.userRepository = userRepository;
         this.userService = userService;
+        this.regRoleMapping = mapRegRoles();
+    }
+
+    private Map<String, String> mapRegRoles() {
+        Map<String, String> roleMap = Map.ofEntries(
+                entry("Registration Software Development Coordinator", "Administrator"),
+                entry("Registration Software Development Manager", "Administrator"),
+                entry("Registration Software Development Staff", "Administrator"),
+                entry("Attendee Registration Coordinator", "Coordinator"),
+                entry("Attendee Registration Coordinator (in Training)", "Coordinator"),
+                entry("Specialty Registration Coordinator", "Coordinator - Specialty Badges"),
+                entry("Specialty and VIP Registration Staff", "Coordinator - VIP Badges"),
+                entry("VIP and Accessibility Registration Coordinator", "Coordinator - VIP Badges"),
+                entry("Assistant Director of Membership", "Director"),
+                entry("Director of Membership", "Director"),
+                entry("Attendee Registration Assistant Manager", "Manager"),
+                entry("Attendee Registration Coordinator Lead", "Manager"),
+                entry("Attendee Registration Manager", "Manager"),
+                entry("Specialty Registration Manager", "Manager"),
+                entry("Staff Registration Check-In Assistant Manager", "Manager"),
+                entry("Staff Registration Check-In Manager", "Manager"),
+                entry("Staff Registration Check-In Coordinator", "MSO"),
+                entry("Staff Registration Check-In Staff", "MSO"),
+                entry("Attendee Registration Staff", "Staff")
+        );
+
+        return roleMap;
     }
 
     // if getting by online id is null, create unique username
     public void createUserFromStaff(StaffImportFile.Person person){
-        User newUser = createNewUser(person);
+        Integer roleId = getStaffRoleId(person.getPositions());
+
+        if(roleId == -1)
+            return;
+
+        User newUser = createNewUser(person, roleId);
         newUser.setOnlineId(person.getId());
 
         if(userRepository.findOneByOnlineId(newUser.getOnlineId()) != null) {
@@ -39,12 +76,26 @@ public class StaffImportUserCreateService {
         saveUser(newUser);
     }
 
-    private User createNewUser(StaffImportFile.Person person) {
+    private User createNewUser(StaffImportFile.Person person, Integer roleId) {
         User newUser = userService.newUser(person.getNamePreferredFirst(), person.getNamePreferredLast());
-        Role staffRole = roleRepository.findByNameIgnoreCase("Staff");
-        newUser.setRoleId(staffRole.getId());
+        newUser.setRoleId(roleId);
 
         return newUser;
+    }
+
+    /**
+     * Checks if position title of Person is present in regRoleMapping
+     * If present, returns roleId associated with the role title
+     * @return roleId or -1 for position that does not need user account created
+     */
+    private Integer getStaffRoleId(List<StaffImportFile.Position> positions) {
+        for(StaffImportFile.Position p : positions) {
+            if(this.regRoleMapping.get(p.title) != null) {
+                return this.roleRepository.findByNameIgnoreCase(this.regRoleMapping.get(p.title)).getId();
+            }
+        }
+
+        return -1;
     }
 
     private void saveUser(User newUser) {
