04588d0778b1fe799a931b77587a152199f9aa87
Jason Short <jason@sheersky.com>
2021-10-31 17:21:33 -0700

    Use privacy name when printing staff badges

--- src/main/java/org/kumoricon/registration/print/BadgePrintService.java
+++ src/main/java/org/kumoricon/registration/print/BadgePrintService.java
@@ -196,12 +196,12 @@ public class BadgePrintService extends PrintService {
         return badgeDTOS;
     }
 
-    private List<StaffBadgeDTO> staffBadgeDTOsFromStaff(@NotNull List<Staff> staffList) {
+    List<StaffBadgeDTO> staffBadgeDTOsFromStaff(@NotNull List<Staff> staffList) {
         List<StaffBadgeDTO> staffBadgeDTOS = new ArrayList<>();
         for (Staff s : staffList) {
             StaffBadgeDTO sb = new StaffBadgeDTO.Builder()
-                    .withFirstName(s.getFirstName())
-                    .withLastName(s.getLastName())
+                    .withFirstName(s.getNamePrivacyFirst()) // Note: Staff's privacy name gets used on the badge
+                    .withLastName(s.getNamePrivacyLast())   // rather than their preferred first/last name
                     .withAgeRange(s.getAgeCategoryAtCon())
                     .withDepartment(s.getDepartment())
                     .withDepartmentBackgroundColor(s.getDepartmentColorCode())
---
+++ src/test/java/org/kumoricon/registration/print/BadgePrintServiceTest.java
@@ -0,0 +1,47 @@
+package org.kumoricon.registration.print;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.kumoricon.registration.model.badge.BadgeService;
+import org.kumoricon.registration.model.staff.BadgeImageService;
+import org.kumoricon.registration.model.staff.BadgeResourceService;
+import org.kumoricon.registration.model.staff.Staff;
+import org.kumoricon.registration.model.staff.StaffBadgeDTO;
+import org.kumoricon.registration.settings.SettingsService;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class BadgePrintServiceTest {
+    BadgePrintService badgePrintService;
+    PrinterInfoService printerInfoService = mock(PrinterInfoService.class);
+    BadgeService badgeService = mock(BadgeService.class);
+    BadgeImageService badgeImageService = mock(BadgeImageService.class);
+    BadgeResourceService badgeResourceService = mock(BadgeResourceService.class);
+    SettingsService settingsService = mock(SettingsService.class);
+
+    @BeforeEach
+    void setUp() {
+        badgePrintService = new BadgePrintService(printerInfoService, badgeService, badgeImageService, badgeResourceService, settingsService);
+    }
+
+    @Test
+    void staffBadgeDTOsFromStaff() {
+        // Staff badges should use the privacy name.
+        // StaffBadgeDTO represents data as it's printed on a badge, where Staff represents
+        // a person. Make sure the Privacy name is used for badges
+        Staff s = new Staff();
+        s.setFirstName("First");
+        s.setLastName("Last");
+        s.setNamePrivacyFirst("PrivacyFirst");
+        s.setNamePrivacyLast("PrivacyLast");
+        s.setBadgeNumber("1234");
+        s.setPositions(List.of());
+
+        List<StaffBadgeDTO> badgeDTOs = badgePrintService.staffBadgeDTOsFromStaff(List.of(s));
+        assertEquals("PrivacyFirst", badgeDTOs.get(0).getFirstName());
+        assertEquals("PrivacyLast", badgeDTOs.get(0).getLastName());
+    }
+}
