c4ac0e86feaf99700c625044eaf027bcae82a562
Alex <53846457+belac9001@users.noreply.github.com>
2022-01-01 21:19:34 -0800

    Add pronoun option (#126)
    * Add he/they & she/they pronoun options
    * Add he/they/she/they pronoun test case

--- src/test/java/org/kumoricon/registration/utility/AttendeeImporterServiceTest.java
+++ src/test/java/org/kumoricon/registration/utility/AttendeeImporterServiceTest.java
@@ -10,8 +10,8 @@ public class AttendeeImporterServiceTest {
     @Test
     public void normalizePronoun() {
         // Valid pronouns can be found in Attendee.PRONOUNS
-        String[] data = {null, "", " ", "she/her", "He/Him", "She/Her", "They/Them", "Ask Me My Pronouns"};
-        String[] expected = {null, null, null, "She/Her", "He/Him", "She/Her", "They/Them", "Ask Me My Pronouns"};
+        String[] data = {null, "", " ", "she/her", "He/Him", "She/Her", "he/they", "She/they", "They/Them", "Ask Me My Pronouns"};
+        String[] expected = {null, null, null, "She/Her", "He/Him", "She/Her", "He/They", "She/They", "They/Them", "Ask Me My Pronouns"};
 
         for (int i = 0; i < data.length; i++) {
             assertEquals(expected[i], AttendeeImporterService.normalizePronoun(data[i]));
