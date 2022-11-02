33e1dd113240a3a450816865608809c361c8fb29
Jason Short <jason@sheersky.com>
2022-11-01 18:56:39 -0700

    Update backup script

--- src/deb/bin/backup.sh
+++ src/deb/bin/backup.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
-# Must be run as a user with postgresql permissions
 
+# Backs up the current registration files and database, saves to
+# YYYYMMDD-HHMMSSPDT-registration.tgz in the current directory
+
+set -e
 export TZ=":America/Los_Angeles"
-TIMESTAMP=`date "+%Y%m%d-%H%M%S%Z"`
+TIMESTAMP=$(date "+%Y%m%d-%H%M%S%Z")
+TARGET=./regbackup
+WORKDIR=/tmp/
+pushd "${WORKDIR}"
+mkdir "${TARGET}"
 
-pg_dump registration > ${TIMESTAMP}-registration.sql
+sudo -u postgres pg_dump registration | tee "${TARGET}/registration.sql" > /dev/null
+cp -r /opt/registration/data/ "${TARGET}/data/"
+cp /opt/registration/*.pem "${TARGET}/" || :   # If file doesn't exist, continue
+# The .pem files are optional, this script should keep working if they don't exist
 
-tar -czvf ${TIMESTAMP}-database.tgz ${TIMESTAMP}-registration.sql
-tar -czvf ${TIMESTAMP}-data.tgz /opt/registration/data/
+tar -czf "${TIMESTAMP}-registration.tgz" "${TARGET}/"
 
-rm ${TIMESTAMP}-registration.sql
+rm -r "${TARGET:?}/"
+popd
+mv "${WORKDIR}/${TIMESTAMP}-registration.tgz" .
+echo "Saved ${TIMESTAMP}-registration.tgz"
+ls -lh "${TIMESTAMP}-registration.tgz"
