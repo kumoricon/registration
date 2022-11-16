26c0a79e63a02433d7306daf9e2a09d1b8260cb9
Jason Short <jason@sheersky.com>
2022-11-15 21:57:49 -0800

    Update Dependency versions - Spring Boot 2.6.1 > 2.7.5 - Bouncycastle 1.6.9 > 1.7.0 - PostgresQL 42.4.1 > 42.5.0 - PdfBox 2.0.24 > 2.0.27 - Junit Jupiter 5.8.2 > 5.9.0   - Had to fix tests

--- src/test/java/org/kumoricon/registration/helpers/AuthHelperTest.java
+++ src/test/java/org/kumoricon/registration/helpers/AuthHelperTest.java
@@ -1,16 +1,14 @@
 package org.kumoricon.registration.helpers;
 
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.kumoricon.registration.model.role.Right;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 
-import javax.servlet.annotation.HttpMethodConstraint;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -18,24 +16,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class AuthHelperTest {
-    public static Stream<Arguments> data() {
-        return Stream.of(
-                Arguments.of("do_things", true),
-                Arguments.of("do_stuff", true),
-                Arguments.of("DO_THINGS", false), // Comparison is case sensitive, by convention rights should be lower case
-                Arguments.of("not_this", false),
-                Arguments.of(null, true)          // If the right is null, assume the user has that right (because there are
-                                                   // no special requirements for that action
-        );
-    }
-
     @ParameterizedTest
-    @MethodSource("data")
+    @ArgumentsSource(MyArgumentsProvider.class)
     public void userHasAuthority(String right, Boolean expected) {
         Authentication auth = new UsernamePasswordAuthenticationToken("user", null,
                         Set.of(new Right("do_things"),
                                 new Right("do_stuff")));
 
         assertEquals(expected, AuthHelper.userHasAuthority(auth, right));
+    }
+
+
+    public static class MyArgumentsProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                Arguments.of("do_things", true),
+                Arguments.of("do_stuff", true),
+                Arguments.of("DO_THINGS", false), // Comparison is case sensitive, by convention rights should be lower case
+                Arguments.of("not_this", false),
+                Arguments.of(null, true)          // If the right is null, assume the user has that right (because there are
+                                                            // no special requirements for that action
+            );
+        }
     }
 }
