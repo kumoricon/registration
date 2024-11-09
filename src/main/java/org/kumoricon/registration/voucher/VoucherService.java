5e46359e6a9339c1356edca9fcb136d156cd86c9
belac9001 <alexkirigiya@gmail.com>
2024-11-09 14:18:04 -0800

    add trade in for voucher history

--- src/main/java/org/kumoricon/registration/voucher/VoucherService.java
+++ src/main/java/org/kumoricon/registration/voucher/VoucherService.java
@@ -5,24 +5,21 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
-import java.time.ZoneId;
 
 @Service
 public class VoucherService {
     private final VoucherRepository voucherRepository;
-    private final ZoneId timezone;
 
     public VoucherService(VoucherRepository voucherRepository) {
         this.voucherRepository = voucherRepository;
-        this.timezone = ZoneId.of("America/Los_Angeles");
     }
 
-    public void saveNewVoucher(final Staff staff, final VoucherType voucherType, final String username) {
+    public void saveNewVoucher(final Staff staff, final VoucherType voucherType, final LocalDate date, final String username) {
         final Voucher voucher = new Voucher();
 
         voucher.setStaffId(staff.getId());
         voucher.setVoucherType(voucherType);
-        voucher.setVoucherDate(LocalDate.now(timezone));
+        voucher.setVoucherDate(date);
         voucher.setVoucherBy(username);
         voucher.setVoucherAt(OffsetDateTime.now());
         voucher.setIsRevoked(false);
